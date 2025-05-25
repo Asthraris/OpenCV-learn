23d1931993b895dc8c4a2fcd33f7ecd02945f0a5
Asthraris <asthraris@gmail.com>
2025-05-25 20:25:29 +0530

    fixed template

--- Resize.py
+++ Resize.py
@@ -2,7 +2,11 @@ import cv2 as cv
 
 #this way i can create moduules for python functions 
 
-#Stored Video ke liye ese resolution change kar sakte hai
+
+
+
+
+#same will have same RATIO in scale
 def cngScaleFrame(frame , scale = 0.5):
     #is [0] se mujhe lagta hai ki opencv bhi 0,0 cordinate image inversion use karta hai as opengl does
     width = int(frame.shape[1] * scale)
@@ -10,16 +14,13 @@ def cngScaleFrame(frame , scale = 0.5):
     dimes = (width,height)
     return cv.resize(frame ,dimes, interpolation=cv.INTER_AREA)
 
+#for UNeven resolution frame
+def cngResolutionFrame(frame , Dimension):
+    return cv.resize(frame,Dimension,interpolation=cv.INTER_AREA)
 
-#only for live video NOT FOR STORED VIDEO
-def cngResolutionFrame(Video ,width,height ):
-    # OpenCv has 3,4 as width and height
-    Video.set(3,width)
-    Video.set(4,height)
-    return Video
 
 #concrete way to find fps and play real duration [[ONLY FOR PRE RECOREDED VIDEOS IN WEBCAM IT WILL GIVE ERROR]]
-def getFrameDuration(Video):
+def getFrameDuration(Video : cv.VideoCapture):
     fps = Video.get(cv.CAP_PROP_FPS)
     frame_count = Video.get(cv.CAP_PROP_FRAME_COUNT)
     duration :int  = (int)(frame_count/fps)
--- reading_vid.py
+++ reading_vid.py
@@ -1,35 +1,45 @@
 import cv2 as cv
-from Resize import cngScaleFrame , cngResolutionFrame
 
-source : str= "Resources/car.mp4"
+#my modules
+from Resize import getFrameDuration
+
+#template jesa bana le
+
+source = "Resources/car.mp4"
+# source = 0
+
 
 #webcam ke liye 0 bola hai , 0 is primaty cam 1 is not available for laptop
-rain_vid = cv.VideoCapture(source)
+vid = cv.VideoCapture(source)
 
 
-
-rain_vid = cngResolutionFrame(rain_vid , 720,720)
-
+#time after which frame are switch
+flipDuration :int
+if isinstance(source,str) :# means local video #isinstance checks the datatype and returns boolean
+    flipDuration = getFrameDuration(vid)
+else:
+    flipDuration = 1
+    
 #iterate over entire frames of vidio
 while True:
     #reads one frame then inc the frame ptr to read next automatically
     # returns success rate bool , and frame data
-    frameSuccess,frame = rain_vid.read()
+    frameSuccess,frame = vid.read()
     if not frameSuccess:
         break
     # imshow mutliple times windows create nhi karta woh name se bas videos ko switch karta hai so names are importnat
-    cv.imshow("Video",cngScaleFrame(frame , 0.5))
+    cv.imshow("Video",frame)
+
+    
 
     # wait for one sec then loop again while loop or accept q key to quit
     # 0->stop of next key
     # 10++ ->slowMotion
-
-
     #webacam me agar 0 rakhte hai tab moment capture hote hai ;]
     #abhi me tukke ka kaam kar raha hu there is no way to play video in same fps as it was rrecoredr
-    if cv.waitKey(duration) & 0xFF==ord('q'):
+    if cv.waitKey(flipDuration) & 0xFF==ord('q'):
         break
 
 #only for Video cache mese buffer ko realese karna padta hai
-rain_vid.release()
+vid.release()
 cv.destroyAllWindows()
